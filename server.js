c94e6277cbbfeedffe2cb6c4de55c370dae3295d
iamausafhussain <drrespect2000@gmail.com>
2022-07-26 12:34:37 +0530

    make it better

--- server.js
+++ server.js
@@ -9,7 +9,5 @@ dotenv.config({ path: "config/config.env" });
 connectDatabase();
 
 app.listen(process.env.PORT, () => {
-  console.log(
-    `Server is listening on port http://localhost:${process.env.PORT}`
-  );
+  console.log(`Server is listening on port ${process.env.PORT}`);
 });
